ea66befdc99b28c9371582702424d4ce78a06d65
zebra1012 <pipyrus94@hotmail.com>
2021-03-08 13:46:44 +0900

    ex03 댓글 갯수(트랜잭션)

--- ex03/src/main/java/org/zerock/service/ReplyServiceImpl.java
+++ ex03/src/main/java/org/zerock/service/ReplyServiceImpl.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyPageDTO;
 import org.zerock.domain.ReplyVO;
+import org.zerock.mapper.BoardMapper;
 import org.zerock.mapper.ReplyMapper;
 
 import lombok.Setter;
@@ -19,9 +21,14 @@ public class ReplyServiceImpl implements ReplyService {
 	@Setter(onMethod_=@Autowired)
 	private ReplyMapper mapper;
 	
+	@Setter(onMethod_=@Autowired)
+	private BoardMapper boardmapper;
+	
+	@Transactional
 	@Override
 	public int register(ReplyVO vo) {
-		log.info("register......."+vo);
+		log.info("register...."+vo);
+		boardmapper.updateReplyCnt(vo.getBno(), 1);
 		return mapper.insert(vo);
 	}
 
@@ -37,9 +44,12 @@ public class ReplyServiceImpl implements ReplyService {
 		return mapper.update(vo);
 	}
 
+	@Transactional
 	@Override
 	public int remove(long rno) {
 		log.info("remove..."+rno);
+		ReplyVO vo = mapper.read(rno);
+		boardmapper.updateReplyCnt(vo.getBno(), -1);
 		return mapper.delete(rno);
 	}
 
